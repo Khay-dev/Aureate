520197836d05073096cad468ff66e095f14fb836
Kalu Daniel <krayzhee419@gmail.com>
2025-02-14 23:01:08 +0100

    Add initial structure for the Contact page with basic layout and styling

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -8,7 +8,7 @@ const ServicePage: React.FC = () => {
 	return (
 		<main className={`${epilogue.className} mt-18 bg-[#FDF0E9]`}>
 			{/* Hero Section */}
-			<section className="bg-[#28293E] text-white  flex flex-col items-center px-5 xl:px-[80px] md:px-[40px] xl:pt-[130px] md:pt-24 lg:pt-36 pt-[7rem] md:pb-16 pb-10 w-full sm:px-2 ">
+			<section className="bg-[#28293E] text-white  flex flex-col items-center px-5 xl:px-[80px] md:px-[40px] xl:pt-[120px] md:pt-24 lg:pt-36 pt-[7rem] md:pb-16 pb-10 w-full sm:px-2 ">
 				<div className=" flex flex-col gap-y-5 items-center justify-center">
 					<p className="xl:text-[72px] lg:text-[60px] md:text-[55px] text-[40px] font-extrabold lg:leading-[80px] tracking-[-2px] text-white">
 						Our Services
