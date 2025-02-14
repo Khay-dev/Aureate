a52917f013624e2692fc60ef06a04df7662ece4a
Kalu Daniel <krayzhee419@gmail.com>
2025-02-14 22:55:24 +0100

    ``` Refactor code structure for improved readability and maintainability ```

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,26 @@
 import type React from 'react';
 
+import { Epilogue } from "next/font/google";
+
+const epilogue = Epilogue({ subsets: ["latin"], weight: ["400", "700"] });
+
 const ContactPage: React.FC = () => {
-  return (
-			<div className="min-h-screen flex items-center justify-center">
-				<h2>Contact Page</h2>
-			</div>
-		);
+	return (
+		<main className={`${epilogue.className} mt-18 bg-[#FDF0E9]`}>
+			{/* Hero Section */}
+			<section className="bg-[#28293E] text-white  flex flex-col items-center px-5 xl:px-[80px] md:px-[40px] xl:pt-[130px] md:pt-24 lg:pt-36 pt-[7rem] md:pb-16 pb-10 w-full sm:px-2 ">
+				<div className=" flex flex-col gap-y-5 items-center justify-center">
+					<p className="xl:text-[72px] lg:text-[60px] md:text-[55px] text-[40px] font-extrabold lg:leading-[80px] tracking-[-2px] text-white">
+						Contact
+					</p>
+					<p className="text-base font-normal text-center leading-8 lg:w-[60%] md:w-[70%] w-full">
+						Agency provides a full service range including technical skills,
+						design, business understanding.
+					</p>
+				</div>
+			</section>
+		</main>
+	);
 };
 
 export default ContactPage;
